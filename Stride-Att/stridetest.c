8de9ee68ab6919b18a2db5f20e03162b9e0dec35
lucien3941 <eclesiastelucien@gmail.com>
2019-11-14 16:38:58 -0200

    script alterado para consumir mais tempo de CPU, assim dá tempo de verificar se o escalonador está funcionando

--- Stride-Att/stridetest.c
+++ Stride-Att/stridetest.c
@@ -4,22 +4,29 @@
 #include "user.h"
 
 
-#define LOOP 1000000
-#define nprocs 3
+#define LOOP 100    //quantidade de iterações que o -for irá executar
+#define nprocs 5	//quantidade de processos que serão criados
 
 
 int main(void){
 
-	for(int i=0; i<nprocs; i++){
-		if(fork((i+1)*100) == 0){
+	for(int i=0; i<nprocs; i++){				//cria a quantidade de processos definidos em nprocs
+		if(fork((i+1)*100) == 0){				//se o processo for filho, executa os loops abaixo
+			int a = 0, b = 10;
 			for(int j=0; j<LOOP; j++){
-				int a = 0, b = 10;
 				a += b;
+				for(int k = 0; k < LOOP; k++){
+					a -= b;
+					printf(0," ");				//gasta processamento dos filhos
+				}
+				printf(1,"\n");	
 			}
+			a = b;
+			b = a;
 			exit();
 		}
 	}
-	for(int x=0; x<nprocs; x++){
+	for(int x=0; x<nprocs; x++){		//espera até o processo terminar
 		wait();
 	}
 
